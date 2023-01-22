223774e572fb0f6da8dbebd3228cda815a4e69f4
Swapnil Wakchaure <swapnilwakchaure2411999@gmail.com>
2023-01-22 19:56:19 +0530

    product_done

--- pepperfry/src/App.js
+++ pepperfry/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import ProductPage from "./Admin_Side/Pages/ProductPage";
 
 function App() {
   return (
     <div className="App">
-      <ProductPage />
     </div>
   );
 }
