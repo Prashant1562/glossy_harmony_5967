1a0e6dfc361a7c2fe47112f0a23004b746f9af56
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-01-22 19:32:18 +0530

    mayuri

--- pepperfry/src/App.js
+++ pepperfry/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Navbar from "./Admin_Side/Components/Mayuri_Home/Navbar/Navbar";
+import Footer from "./Admin_Side/Components/Mayuri_Home/Navbar/Footer"
 import './App.css';
+import AllRoutes from "./Admin_Side/Pages/AllRoutes";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      
+      <AllRoutes />
+      <Footer />
     </div>
   );
 }
