729b2a1bc8135a19ce1dc3965c859ac829224419
Prashant_Rao <prashantraogunjal1562@gmail.com>
2023-01-22 20:25:52 +0530

    a

--- pepperfry/src/App.js
+++ pepperfry/src/App.js
@@ -1,12 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import CardPayment from "./Admin_Side/Components/Prashant_Checkout/CardPayment/CardPayment"
-import Success from "./Admin_Side/Components/Prashant_Checkout/SucessPage/Success"
+
 function App() {
   return (
     <div className="App">
-  <CardPayment />
-  <Success />
     </div>
   );
 }
